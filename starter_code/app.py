bd5753cbf92fa12e2ac27a8a13b44defc6b688bc
Waleed <waleedghaleb@gmail.com>
2025-11-15 16:18:55 -0800

    forgot to add the api

--- starter_code/app.py
+++ starter_code/app.py
@@ -21,6 +21,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
