d16d7380431441c27a15982af201e342afbfcab5
Waleed <waleedghaleb@gmail.com>
2025-11-15 16:18:05 -0800

    created UserRegister

--- starter_code/app.py
+++ starter_code/app.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.user import UserRegister
 
 app = Flask(__name__)
 
